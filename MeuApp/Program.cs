039457cea07c42996b891e159aad218f7e9c9431
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-09 15:58:10 -0300

    add test program

---
+++ MeuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Namespace is a logic division
+namespace MeuApp
+{
+    // Main class
+    class Program
+    {
+        // Main entry point
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
